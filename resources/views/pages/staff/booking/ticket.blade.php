77ee81e8fc07f95e3c86ea6d3d32d624165f69ca
hazimah <hazimahpethie@uitm.edu.my>
2024-08-22 17:18:20 +0800

    Updated ticket info

--- resources/views/pages/staff/booking/ticket.blade.php
+++ resources/views/pages/staff/booking/ticket.blade.php
@@ -34,10 +34,12 @@
                             </table>
 
                             <div class="alert alert-info mt-0">
-                                <strong>Nota:</strong> Sila tunjukkan tiket ini kepada urusetia ketika hadir ke Malam Gala.
+                                <strong>Nota:</strong> Sila tunjukkan tiket ini kepada urusetia ketika hadir ke Malam
+                                Gala.
                             </div>
                             <div class="text-center">
-                                <a href="{{ route('staff.booking.print', $booking->id) }}" class="btn btn-primary w-100 mt-0" target="_blank">
+                                <a href="{{ route('staff.booking.print', $booking->id) }}"
+                                    class="btn btn-primary w-100 mt-0" target="_blank">
                                     <i class="fas fa-download"></i> Muat Turun Tiket
                                 </a>
                             </div>
@@ -47,13 +49,17 @@
                                 <div class="accordion" id="layoutAccordion">
                                     <div class="accordion-item">
                                         <h4 class="accordion-header" id="headingOne">
-                                            <button class="accordion-button text-uppercase" type="button" data-bs-toggle="collapse" data-bs-target="#collapseLayout" aria-expanded="true" aria-controls="collapseLayout">
+                                            <button class="accordion-button text-uppercase" type="button"
+                                                data-bs-toggle="collapse" data-bs-target="#collapseLayout"
+                                                aria-expanded="true" aria-controls="collapseLayout">
                                                 Paparan Pelan Meja Sebenar
                                             </button>
                                         </h4>
-                                        <div id="collapseLayout" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#layoutAccordion">
+                                        <div id="collapseLayout" class="accordion-collapse collapse"
+                                            aria-labelledby="headingOne" data-bs-parent="#layoutAccordion">
                                             <div class="accordion-body">
-                                                <img src="{{ asset('public/assets/images/layout.jpg') }}" alt="Pelan Meja" class="img-fluid">
+                                                <img src="{{ asset('public/assets/images/layout.jpg') }}"
+                                                    alt="Pelan Meja" class="img-fluid">
                                             </div>
                                         </div>
                                     </div>
@@ -67,25 +73,29 @@
                                 <!-- Tables -->
                                 <div class="row justify-content-center mt-4">
                                     @foreach ($tables as $table)
-                                    <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center mb-4">
-                                        <div class="table-round p-3 
-            @if ($table->id == $booking->table_id) bg-warning 
-            @elseif ($table->available_seat > 0) bg-success
-            @else bg-secondary text-light @endif"
-                                            id="table-container-{{ $table->id }}">
-                                            <div>
-                                                <label class="text-dark">
-                                                    <strong>{{ $table->table_no }}</strong>
-                                                </label>
-                                            </div>
-                                            <div class="table-info">
-                                                <p>{{ $table->available_seat }} kerusi kosong</p>
-                                                @if ($table->id == $booking->table_id)
-                                                <p style="font-size: 10px; font-weight:bold">{{ $booking->staff->name }}</p>
-                                                @endif
-                                            </div>
-                                        </div>
-                                    </div>
+                                                                <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center mb-4">
+                                                                    <div class="table-round p-3 
+                                        @if ($table->id == $booking->table_id) bg-warning 
+                                        @elseif ($table->available_seat > 0) bg-success
+                                        @else bg-secondary text-light @endif" id="table-container-{{ $table->id }}">
+                                                                        <div>
+                                                                            <label class="text-dark">
+                                                                                <strong>{{ $table->table_no }}</strong>
+                                                                            </label>
+                                                                        </div>
+                                                                        <div class="table-info">
+                                                                            @if ($table->available_seat == 0)
+                                                                                <p>Penuh</p>
+                                                                            @else
+                                                                                <p>{{ $table->available_seat }} kerusi kosong</p>
+                                                                            @endif
+                                                                            @if ($table->id == $booking->table_id)
+                                                                                <p style="font-size: 10px; font-weight:bold">{{ $booking->staff->name }}
+                                                                                </p>
+                                                                            @endif
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
                                     @endforeach
                                 </div>
                             </div>
