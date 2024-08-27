a1c494d5ef93b36b4ad079ed31563ae3a1cf91ab
hazimah <hazimahpethie@uitm.edu.my>
2024-08-28 00:07:52 +0800

    update trash list attendance

--- resources/views/pages/attendance/trash.blade.php
+++ resources/views/pages/attendance/trash.blade.php
@@ -2,21 +2,21 @@
 @section('content')
 <!--breadcrumb-->
 <div class="page-breadcrumb d-none d-sm-flex align-items-center mb-3">
-    <div class="breadcrumb-title pe-3">Pengurusan Meja</div>
+    <div class="breadcrumb-title pe-3">Pengurusan Kehadiran</div>
     <div class="ps-3">
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb mb-0 p-0">
                 <li class="breadcrumb-item"><a href="{{ route('home') }}"><i class="bx bx-home-alt"></i></a>
                 </li>
-                <li class="breadcrumb-item"><a href="{{ route('table') }}"></i>Senarai Meja</a>
+                <li class="breadcrumb-item"><a href="{{ route('attendance') }}"></i>Senarai Kehadiran</a>
                 </li>
-                <li class="breadcrumb-item active" aria-current="page">Senarai Meja Dipadam</li>
+                <li class="breadcrumb-item active" aria-current="page">Senarai Kehadiran Dipadam</li>
             </ol>
         </nav>
     </div>
 </div>
 <!--end breadcrumb-->
-<h6 class="mb-0 text-uppercase">Senarai Meja Dipadam</h6>
+<h6 class="mb-0 text-uppercase">Senarai Kehadiran Dipadam</h6>
 <hr />
 <div class="card">
     <div class="card-body">
@@ -24,43 +24,38 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th>No. Meja</th>
-                        <th>Jumlah Tempat Duduk</th>
-                        <th>Jumlah Tempat Duduk Kosonge</th>
-                        <th>Status</th>
+                        <th>#</th>
+                        <th>Nama</th>
+                        <th>No. Pekerja</th>
+                        <th>No. Tempahan</th>
+                        <th>Jenis Pengguna</th>
+                        <th>Tarikh Imbas</th>
                         <th>Tindakan</th>
                     </tr>
                 </thead>
                 <tbody>
                     @if (count($trashList) > 0)
-                    @foreach ($trashList as $table)
-                    <tr>
-                        <td>{{ $table->table_no }}</td>
-                        <td>{{ $table->total_seat }}</td>
-                        <td>{{ $table->available_seat }}</td>
-                        <td>
-                            @if ($table->status == 'Tersedia')
-                            <span class="badge bg-success">Tersedia</span>
-                            @else
-                            <span class="badge bg-warning">Penuh</span>
-                            @endif
-                        </td>
-                        <td>
-                            <a href="{{ route('table.restore', $table->id) }}" class="btn btn-success btn-sm"
-                                data-bs-toggle="tooltip" data-bs-placement="bottom" title="Kembalikan">
-                                <i class="bx bx-undo"></i>
-                            </a>
-                            <a type="button" data-bs-toggle="tooltip" data-bs-placement="bottom"
-                                data-bs-title="Padam">
-                                <span class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                    data-bs-target="#deleteModal{{ $table->id }}"><i
-                                        class="bx bx-trash"></i></span>
-                            </a>
-                        </td>
-                    </tr>
-                    @endforeach
+                        @foreach ($trashList as $attendance)
+                        <tr>
+                                    <td>{{ ($trashList->currentPage() - 1) * $trashList->perPage() + $loop->iteration }}
+                                    </td>
+                                    <td>{{ $attendance->staff->name }}</td>
+                                    <td>{{ $attendance->staff->no_pekerja }}</td>
+                                    <td>{{ $attendance->staff->booking->booking_no }}</td>
+                                    <td>{{ $attendance->staff->type }}</td>
+                                    <td>{{ $attendance->check_in }}</td>
+                                    <td>
+                                        <a type="button" data-bs-toggle="tooltip" data-bs-placement="bottom"
+                                            data-bs-title="Padam">
+                                            <span class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                                data-bs-target="#deleteModal{{ $attendance->id }}"><i
+                                                    class="bx bx-trash"></i></span>
+                                        </a>
+                                    </td>
+                                </tr>
+                        @endforeach
                     @else
-                    <td colspan="4">Tiada rekod</td>
+                        <td colspan="4">Tiada rekod</td>
                     @endif
                 </tbody>
             </table>
@@ -68,7 +63,7 @@
         <div class="mt-3 d-flex justify-content-between">
             <div class="d-flex align-items-center">
                 <span class="mr-2 mx-1">Jumlah rekod per halaman</span>
-                <form action="{{ route('table') }}" method="GET" id="perPageForm">
+                <form action="{{ route('attendance') }}" method="GET" id="perPageForm">
                     <select name="perPage" id="perPage" class="form-select"
                         onchange="document.getElementById('perPageForm').submit()">
                         <option value="10" {{ Request::get('perPage') == '10' ? 'selected' : '' }}>10</option>
@@ -90,35 +85,35 @@
 
 <!-- Delete Confirmation Modal -->
 @foreach ($trashList as $table)
-<div class="modal fade" id="deleteModal{{ $table->id }}" tabindex="-1" aria-labelledby="deleteModalLabel"
-    aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="deleteModalLabel">Pengesahan Padam Rekod</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                @isset($table)
-                Adakah anda pasti ingin memadam rekod <span style="font-weight: 600;">
-                    {{ $table->table_no }}</span>?
-                @else
-                Tiada rekod
-                @endisset
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                @isset($table)
-                <form class="d-inline" method="POST" action="{{ route('table.forceDelete', $table->id) }}">
-                    {{ method_field('delete') }}
-                    {{ csrf_field() }}
-                    <button type="submit" class="btn btn-danger">Padam</button>
-                </form>
-                @endisset
+    <div class="modal fade" id="deleteModal{{ $table->id }}" tabindex="-1" aria-labelledby="deleteModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">Pengesahan Padam Rekod</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    @isset($table)
+                        Adakah anda pasti ingin memadam rekod <span style="font-weight: 600;">
+                            {{ $table->staff->name }} ({{ $table->staff->no_pekerja }})</span>?
+                    @else
+                        Tiada rekod
+                    @endisset
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+                    @isset($table)
+                        <form class="d-inline" method="POST" action="{{ route('attendance.forceDelete', $table->id) }}">
+                            {{ method_field('delete') }}
+                            {{ csrf_field() }}
+                            <button type="submit" class="btn btn-danger">Padam</button>
+                        </form>
+                    @endisset
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endforeach
 <!--end page wrapper -->
 @endsection
